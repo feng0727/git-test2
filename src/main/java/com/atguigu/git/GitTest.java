deff65d9ca701340b6a891e9041af21a2c047fa6
xuebing feng <fengxb@broada.com>
2023-04-05 21:54:01 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello gitee");
         System.out.println("hello gitee1");
+        System.out.println("master test");
     }
 
 
