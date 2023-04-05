8ff7373ccbc8de04c03aa540199d4b9d3f621fdc
xuebing feng <fengxb@broada.com>
2023-04-05 21:48:21 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello gitee");
+        System.out.println("hello gitee1");
     }
 
 
