e28dd72bb5ae20f2c547381760124b14f98668ef
xuebing feng <fengxb@broada.com>
2024-07-02 14:50:31 +0800

    修改为Springboot项目

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello gitee1");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test-2024-07-02");
     }
 
 
---
+++ src/main/java/com/atguigu/git/GitTest02.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitTest02 {
+
+
+
+
+}
