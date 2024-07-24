4af8d5a7007679274f092f997185c90c4b05db67
xuebing feng <fungxuebing@163.com>
2024-07-24 11:27:37 +0800

    测试tag

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,9 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("hot-fix test-2024-07-02");
         System.out.println("hot-fix test-2024-07-02_01");
+
+
+        System.out.println("hot-fix test-2024-07-24_01");
     }
 
 
