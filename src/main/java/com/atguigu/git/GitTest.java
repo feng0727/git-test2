31fbc16790dc9aa88eb84363ab89143efaaa999d
xuebing feng <fungxuebing@163.com>
2024-07-24 11:01:53 +0800

    添加一个新的输出

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("hot-fix test-2024-07-02");
+        System.out.println("hot-fix test-2024-07-02_01");
     }
 
 
